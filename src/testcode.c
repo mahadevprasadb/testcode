ed250e9bb73e88c39454a35534c6e3695893651e
mahadevprasadb <prasad.m6b@gmail.com>
2023-10-29 23:31:10 +0530

    Abstract RNG initialization

--- src/testcode.c
+++ src/testcode.c
@@ -3,13 +3,18 @@
 #include <time.h>
 int random_int(int max)
 {
-return rand() % max;
+ return rand() % max;
+}
+void init_rand(void)
+{
+  strand(time(NULL));
 }
 int main(int argc, char *argv[])
 {
-if (argc != 2) {
-fprintf(stderr, "Usage:: %s <number>\n", argv[0]);
-return EXIT_FAILURE;
+ if (argc != 2)
+{
+ fprintf(stderr, "Usage:: %s <number>\n", argv[0]);
+ return EXIT_FAILURE;
 }
 int max = atoi(argv[1]);
 int result = random_int(max);
